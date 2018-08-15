335de7e576cb473dc08d254b3b84af8ee8e0879e
Mark Hellar <mark.hellar@gmail.com>
2018-08-14 22:48:27 -0700

    first

---
+++ day1/code/osc/index.js
@@ -0,0 +1,7 @@
+var osc = require('node-osc');
+                
+var oscServer = new osc.Server(9999, '0.0.0.0');
+oscServer.on("message", function(msg, rinfo) {
+    console.log("OSC message:");
+    console.log(msg);
+});
